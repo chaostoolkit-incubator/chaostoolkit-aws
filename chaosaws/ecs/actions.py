d776b45e09c8e147186aeda7881fe678a30eafc2
Brian Devins <brian.devins@coxautoinc.com>
2018-01-31 11:42:28 -0500

    Initial action, need a probe for service steady state also
    Signed-off-by: Brian Devins <brian.devins@coxautoinc.com>

--- chaosaws/ecs/actions.py
+++ chaosaws/ecs/actions.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
-__all__ = []
+from chaoslib.types import Configuration, Secrets
+
+from chaosaws import aws_client
+from chaosaws.types import AWSResponse
+
+__all__ = ["stop_task"]
+
+
+def stop_task(cluster: str,
+              task_id: str,
+              configuration: Configuration=None,
+              secrets: Secrets=None) -> AWSResponse:
+    """
+    Stop a given ECS task instance
+    """
+    client = aws_client("ecs", configuration, secrets)
+    return client.stop_task(cluster=cluster, task=task_id, reason='Chaos Testing')
