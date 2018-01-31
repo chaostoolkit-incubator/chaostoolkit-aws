5de6644dbba5fb32872aa5ffa2c50d6dab647f7c
Brian Devins <brian.devins@coxautoinc.com>
2018-01-31 11:51:49 -0500

    Make reason configurable and shorten the line that was too long
    Signed-off-by: Brian Devins <brian.devins@coxautoinc.com>

--- chaosaws/ecs/actions.py
+++ chaosaws/ecs/actions.py
@@ -9,10 +9,11 @@ __all__ = ["stop_task"]
 
 def stop_task(cluster: str,
               task_id: str,
+              reason='Chaos Testing',
               configuration: Configuration=None,
               secrets: Secrets=None) -> AWSResponse:
     """
     Stop a given ECS task instance
     """
     client = aws_client("ecs", configuration, secrets)
-    return client.stop_task(cluster=cluster, task=task_id, reason='Chaos Testing')
+    return client.stop_task(cluster=cluster, task=task_id, reason=reason)
