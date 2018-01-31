056be051b1220bdf61fbc0cd88b07c9930713709
Brian Devins <brian.devins@coxautoinc.com>
2018-01-31 14:37:51 -0500

    Add tests for the probe. Renamed the method to not be redundant
    Signed-off-by: Brian Devins <brian.devins@coxautoinc.com>

--- chaosaws/ecs/probes.py
+++ chaosaws/ecs/probes.py
@@ -3,10 +3,10 @@ from chaoslib.types import Configuration, Secrets
 
 from chaosaws import aws_client
 
-__all__ = ["ecs_service_is_not_deploying"]
+__all__ = ["service_is_not_deploying"]
 
 
-def ecs_service_is_not_deploying(cluster: str,
+def service_is_not_deploying(cluster: str,
                                  service: str,
                                  configuration: Configuration = None,
                                  secrets: Secrets = None) -> bool:
--- tests/ecs/test_ecs_probes.py
+++ tests/ecs/test_ecs_probes.py
@@ -1,1 +1,41 @@
 # -*- coding: utf-8 -*-
+from unittest.mock import MagicMock, patch
+
+from chaosaws.ecs.probes import service_is_not_deploying
+
+
+@patch('chaosaws.ecs.probes.aws_client', autospec=True)
+def test_ecs_service_is_not_deploying(aws_client):
+    client = MagicMock()
+    client.describe_services = MagicMock(return_value={
+        'services': {
+            'deployments': [
+                {"status": "PRIMARY"}
+            ]
+        }
+    })
+    aws_client.return_value = client
+    cluster = "ecs-cluster"
+    service = "ecs-service"
+    response = service_is_not_deploying(cluster, service)
+    client.describe_services.assert_called_with(cluster=cluster, services=[service])
+    assert response is True
+
+
+@patch('chaosaws.ecs.probes.aws_client', autospec=True)
+def test_ecs_service_is_deploying(aws_client):
+    client = MagicMock()
+    client.describe_services = MagicMock(return_value={
+        'services': {
+            'deployments': [
+                {'status': 'PRIMARY'},
+                {'status': 'ACTIVE'}
+            ]
+        }
+    })
+    aws_client.return_value = client
+    cluster = "ecs-cluster"
+    service = "ecs-service"
+    response = service_is_not_deploying(cluster, service)
+    client.describe_services.assert_called_with(cluster=cluster, services=[service])
+    assert response is False
