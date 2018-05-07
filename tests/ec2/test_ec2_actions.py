bf5c6e06e42949b61a3ad44e2e5d596b9a2550fc
Antoine Carat <antoine.carat@gmail.com>
2018-05-07 09:11:27 +0200

    Add random stop, multiple stop, random in a given AZ and entire AZ (#8)
    * Add random stop, multiple stop, random in a given AZ and entire AZ
    
    Signed-off-by: Antoine Carat <acarat@oui.sncf>

--- tests/ec2/test_ec2_actions.py
+++ tests/ec2/test_ec2_actions.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from unittest.mock import MagicMock, patch
 
-from chaosaws.ec2.actions import stop_instance
-
+from chaosaws.ec2.actions import stop_instance, stop_instances
 
 @patch('chaosaws.ec2.actions.aws_client', autospec=True)
 def test_stop_instance(aws_client):
@@ -22,3 +21,13 @@ def test_stop_instance_can_be_forced(aws_client):
     response = stop_instance(inst_id, force=True)
     client.stop_instances.assert_called_with(
         InstanceIds=[inst_id], Force=True)
+
+
+@patch('chaosaws.ec2.actions.aws_client', autospec=True)
+def test_stop_instances(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    inst_ids = ["i-1234567890abcdef0", "i-987654321fedcba"]
+    response = stop_instances(inst_ids)
+    client.stop_instances.assert_called_with(
+        InstanceIds=inst_ids, Force=False)
