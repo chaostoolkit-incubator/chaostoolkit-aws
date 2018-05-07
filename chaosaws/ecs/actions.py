f5231298dc96f1838973fa77b4c52b458079f104
Antoine Carat <acarat@oui.sncf>
2018-05-07 17:02:05 +0200

    Merge delete_service and delete_services functions
    Signed-off-by: Antoine Carat <acarat@oui.sncf>

--- chaosaws/ecs/actions.py
+++ chaosaws/ecs/actions.py
@@ -26,73 +26,50 @@ def stop_task(cluster: str,
 
 
 def delete_service(service: str = None, cluster: str = None,
+                   service_pattern: str = None,
                    configuration: Configuration = None,
                    secrets: Secrets = None) -> AWSResponse:
     """
-    Update a given ECS service by updating it to set the desired
-    count of tasks to 0 then delete it.
+    Update a given ECS service by updating it to set the desired count of tasks
+    to 0 then delete it. If not provided, a random one will be picked up
+    regarding `service_pattern`, if provided, so that only service names
+    matching the pattern would be be used. This should be a valid regex.
 
     You can specify a cluster by its ARN identifier or, if not provided, the
     default cluster will be picked up.
     """
     client = aws_client("ecs", configuration, secrets)
+    if not service:
+        res = client.list_services(cluster=cluster, maxResults=10)
+        services = res["serviceArns"][:]
+        while True:
+            next_token = res.get("nextToken")
+            if not next_token:
+                break
 
-    logger.debug("Updating ECS service: {}".format(service))
-    client.update_service(
-        cluster=cluster, service=service, desiredCount=0,
-        deploymentConfiguration={
-            'maximumPercent': 100,
-            'minimumHealthyPercent': 0
-        })
-
-    logger.debug("Deleting ECS service: {}".format(service))
-    return client.delete_service(cluster=cluster, service=service)
-
-
-def delete_services(cluster: str = None, service_pattern: str = None,
-                    configuration: Configuration = None,
-                    secrets: Secrets = None) -> AWSResponse:
-    """
-    Update a given ECS service by updating each service to set the desired
-    count of tasks to 0 then delete it.
-
-    You may set `service_pattern` so that only service names matching the
-    pattern would be be used. This should be a valid regex.
-
-    You can specify a cluster by its ARN identifier or, if not provided, the
-    default cluster will be picked up.
-    """
-    client = aws_client("ecs", configuration, secrets)
-
-    res = client.list_services(cluster=cluster, maxResults=10)
-    services = res["serviceArns"][:]
-    while True:
-        next_token = res.get("nextToken")
-        if not next_token:
-            break
-
-        res = client.list_services(
-            cluster=cluster, nextToken=next_token, maxResults=10)
-        services.extend(res["serviceArns"])
-
-    if not services:
-        raise FailedActivity("No ECS services found")
-
-    # should we filter the number of services to take into account?
-    if service_pattern:
-        r = re.compile(service_pattern)
-        filtered = []
-        for service in services:
-            if r.search(service):
-                filtered.append(service)
-        services = filtered
+            res = client.list_services(
+                cluster=cluster, nextToken=next_token, maxResults=10)
+            services.extend(res["serviceArns"])
 
         if not services:
-            raise FailedActivity(
-                "No ECS services matching pattern: {}".format(service_pattern))
+            raise FailedActivity("No ECS services found")
 
-    service = random.choice(services)
-    service = service.rsplit("/", 1)[1]
+        # should we filter the number of services to take into account?
+        if service_pattern:
+            r = re.compile(service_pattern)
+            filtered = []
+            for service in services:
+                if r.search(service):
+                    filtered.append(service)
+            services = filtered
+
+            if not services:
+                raise FailedActivity(
+                    "No ECS services matching pattern: {}".format(
+                        service_pattern))
+
+        service = random.choice(services)
+        service = service.rsplit("/", 1)[1]
 
     logger.debug("Updating ECS service: {}".format(service))
     client.update_service(cluster=cluster, service=service,
