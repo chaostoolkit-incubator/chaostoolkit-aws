5b69bb4c2cf06d3b68862964769b33107bc4fab0
Dmitry Batiievskyi <dbat@softserveinc.com>
2018-08-09 18:15:27 +0300

    refactoring for spot instances support (#15)
    Signed-off-by: Dmitry Batiievskyi <dmitriy@batiyevsky.org>

--- chaosaws/ec2/probes.py
+++ chaosaws/ec2/probes.py
@@ -6,7 +6,7 @@ from chaoslib.types import Configuration, Secrets
 from chaosaws import aws_client
 from chaosaws.types import AWSResponse
 
-__all__ = ["describe_instances"]
+__all__ = ["describe_instances", "count_instances"]
 
 
 def describe_instances(filters: List[Dict[str, Any]],
@@ -20,3 +20,17 @@ def describe_instances(filters: List[Dict[str, Any]],
     """  # noqa: E501
     client = aws_client('ec2', configuration, secrets)
     return client.describe_instances(Filters=filters)
+
+
+def count_instances(filters: List[Dict[str, Any]],
+                    configuration: Configuration = None,
+                    secrets: Secrets = None) -> AWSResponse:
+    """
+    Return count of instances matching the specified filters.
+
+    Please refer to http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.describe_instances
+    for details on said filters.
+    """  # noqa: E501
+    client = aws_client('ec2', configuration, secrets)
+    result = client.describe_instances(Filters=filters)
+    return (len(result['Reservations']))
