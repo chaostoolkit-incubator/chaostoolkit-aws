dd3972388224e8b7bd261b87407a0578a3bc19c9
Joshua Root <joshua.root@capitalone.com>
2020-06-01 15:01:31 -0400

    adding ecs monitor probe
    - adding new probe "monitor" to ecs
    - monitor can invoke any other probe and will wait for specified criteria to determine if it has been disrupted or has recovered.
    - if the monitor times out (default 300s) it will throw a FailedActivity

--- chaosaws/utils.py
+++ chaosaws/utils.py
@@ -1,4 +1,18 @@
 #
+import jmespath
+from typing import Union
+
+
 def breakup_iterable(values: list, limit: int = 50) -> list:
     for i in range(0, len(values), limit):
         yield values[i:min(i + limit, len(values))]
+
+
+def jmes_search(json_path: str,
+                data: dict,
+                value: Union[list, int, bool, str]) -> bool:
+    if isinstance(value, int):
+        results = jmespath.search('%s | length(@)' % json_path, data)
+    else:
+        results = jmespath.search(json_path, data)
+    return results == value
