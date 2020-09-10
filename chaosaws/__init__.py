b8bfa23295cdbbf451bd3741268dbaa3c7b05298
luismenco <luismenco50@gmail.com>
2020-09-10 14:51:56 -0500

    adding actions for aws ssm
    Signed-off-by: luismenco <luismenco50@gmail.com>

--- chaosaws/__init__.py
+++ chaosaws/__init__.py
@@ -240,5 +240,6 @@ def load_exported_activities() -> List[DiscoveredActivities]:
     activities.extend(discover_probes("chaosaws.rds.probes"))
     activities.extend(discover_actions("chaosaws.elasticache.actions"))
     activities.extend(discover_probes('chaosaws.elasticache.probes'))
+    activities.extend(discover_probes('chaosaws.ssm.actions'))
 
     return activities
---
+++ tests/ssm/test_ssm_actions.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from unittest.mock import MagicMock, patch,mock_open
+import os
+import pytest
+from chaoslib.exceptions import FailedActivity
+from chaosaws.ssm.actions import (create_document, send_command,
+                                  delete_document)
+
+
+@patch('chaosaws.ssm.actions.aws_client', autospec=True)
+def test_create_documente_with_params_required_empty(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    name=''
+    content=''
+    with pytest.raises(FailedActivity):
+        create_document(path_content=content,name=name)
+
+
+@patch('chaosaws.ssm.actions.aws_client', autospec=True)
+def test_send_command_with_params_required_empty(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    document_name=''
+    with pytest.raises(FailedActivity):
+        send_command(document_name=document_name)
+
+@patch('chaosaws.ssm.actions.aws_client', autospec=True)
+def test_send_command(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    document_name='some_document_name'
+    send_command(document_name=document_name,document_version='some_Document_version',max_concurrency="1",max_errors="2",parameters={'value':'some_value'},region='some_region')
+    client.send_command(DocumentName=document_name,DocumentVersion='some_Document_version',MaxConcurrency="1",MaxErrors="2",Parameters=[{'value':'some_value'}],Region='some_region')
+
+
+@patch('chaosaws.ssm.actions.aws_client', autospec=True)
+def test_delete_document_with_params_required_empty(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    name=''
+    with pytest.raises(FailedActivity):
+        delete_document(name=name)
+
+@patch('chaosaws.ssm.actions.aws_client', autospec=True)
+def test_delete_document(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    name='some_name'
+    delete_document(name=name,version_name='some_version',force='some_force')
+    client.delete_document(Name=name,DocumentVersion='some_version',Force='some_force')
+
