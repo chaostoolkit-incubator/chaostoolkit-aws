6e031f4e1a0e4ea5f1fe8a188134725475c6a211
Ciaran Evans <ciaran@reliably.com>
2021-08-12 15:12:52 +0100

    Add unit tests
    Signed-off-by: Ciaran Evans <ciaran@reliably.com>

---
+++ tests/fis/test_fis_actions.py
@@ -0,0 +1,78 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from chaoslib.exceptions import FailedActivity
+
+from chaosaws.fis.actions import start_experiment
+
+
+def test_that_fis_module_exposes_correct_all_attributes():
+    import chaosaws.fis.actions as actions
+    all = actions.__all__
+    assert "start_experiment" in all
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_start_experiment_invoked_correctly_if_only_given_template_id(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+
+    start_experiment(experiment_template_id="an-id")
+    client.start_experiment.assert_called_once_with(experimentTemplateId="an-id")
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_start_experiment_invoked_correctly_if_given_all_params(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+
+    start_experiment(
+        experiment_template_id="an-id",
+        client_token="a-token",
+        tags={"test-tag": "a-value"},
+    )
+    client.start_experiment.assert_called_once_with(
+        experimentTemplateId="an-id",
+        clientToken="a-token",
+        tags={"test-tag": "a-value"},
+    )
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_start_experiment_fails_if_template_id_empty_or_none(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+
+    with pytest.raises(FailedActivity) as ex:
+        start_experiment(experiment_template_id="")
+    assert str(ex.value) == (
+        "You must pass a valid experiment template id, id provided was empty"
+    )
+
+    with pytest.raises(FailedActivity) as ex:
+        start_experiment(experiment_template_id=None)
+    assert str(ex.value) == (
+        "You must pass a valid experiment template id, id provided was empty"
+    )
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_start_experiment_fails_if_exception_raised(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    client.start_experiment.side_effect = Exception("Something went wrong")
+
+    with pytest.raises(FailedActivity) as ex:
+        start_experiment(experiment_template_id="an-id")
+    assert str(ex.value) == "Start Experiment failed, reason was: Something went wrong"
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_start_experiment_returns_client_response(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    resp = {"a-key": "a-value"}
+    client.start_experiment.return_value = resp
+
+    actual_resp = start_experiment(experiment_template_id="an-id")
+    assert actual_resp == resp
