dff5fd43e4ff663e188b2f6152aa2c4e0a7ea171
Ciaran Evans <ciaran@reliably.com>
2021-08-12 15:30:49 +0100

    Add docstrings
    Signed-off-by: Ciaran Evans <ciaran@reliably.com>

--- chaosaws/fis/actions.py
+++ chaosaws/fis/actions.py
@@ -16,6 +16,40 @@ def start_experiment(
     configuration: Configuration = None,
     secrets: Secrets = None,
 ) -> AWSResponse:
+    """
+    Starts running an experiment from the specified experiment template.
+
+    :param experiment_template_id: str representing the id of the experiment template
+        to run
+    :param client_token: str representing the unique identifier for this experiment run.
+        If a value is not provided, boto3 generates one for you
+    :param tags: Dict[str, str] representing tags to apply to the experiment that is
+        started
+    :param configuration: Configuration object representing the CTK Configuration
+    :param secrets: Secret object representing the CTK Secrets
+    :returns: AWSResponse representing the response from FIS upon starting the
+        experiment
+
+    Examples
+    --------
+    >>> start_experiment(
+    ...     experiment_template_id="EXT6oWVA1WrLNy4XS"
+    ... )
+    {
+    'ResponseMetadata': {'RequestId': '1ceaedae-5897-4b64-9ade-9e94449f1262',
+    'HTTPStatusCode': 200, 'HTTPHeaders': {'date': 'Thu, 12 Aug 2021 14:21:19 GMT',
+    ...
+    'experiment': {'id': 'EXPXDPecuQBFiZs1Jz',
+    'experimentTemplateId': 'EXT6oWVA1WrLNy4XS',
+    ...
+    }
+
+    >>> start_experiment(
+    ...     experiment_template_id="EXT6oWVA1WrLNy4XS",
+    ...     client_token="my-unique-token",
+    ...     tags={"a-key": "a-value"}
+    ... )
+    """
 
     if not experiment_template_id:
         raise FailedActivity(
