b428f3baa1e5fb5b86c51c0589d0dc5b9d8f7fc6
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2021-08-16 10:42:13 +0100

    Add `chaosaws.fis.actions.stop_experiment` (#117)
    adds the chaosaws.fis.actions.stop_experiment function to stop AWS FIS experiments
    Signed-off-by: Ciaran Evans <ciaran@reliably.com>

--- tests/fis/test_fis_actions.py
+++ tests/fis/test_fis_actions.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 from chaoslib.exceptions import FailedActivity
 
-from chaosaws.fis.actions import start_experiment
+from chaosaws.fis.actions import start_experiment, stop_experiment
 
 
 def test_that_fis_modules___all___attribute_exposed_correctly():
@@ -11,6 +11,7 @@ def test_that_fis_modules___all___attribute_exposed_correctly():
 
     all = actions.__all__
     assert "start_experiment" in all
+    assert "stop_experiment" in all
 
 
 @patch("chaosaws.fis.actions.aws_client", autospec=True)
@@ -76,4 +77,53 @@ def test_that_start_experiment_returns_client_response(aws_client):
     client.start_experiment.return_value = resp
 
     actual_resp = start_experiment(experiment_template_id="an-id")
+    assert actual_resp == resp
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_stop_experiment_invoked_correctly_when_given_experiment_id(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+
+    stop_experiment(experiment_id="an-id")
+    client.stop_experiment.assert_called_once_with(id="an-id")
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_stop_experiment_fails_if_experiment_id_empty_or_none(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+
+    with pytest.raises(FailedActivity) as ex:
+        stop_experiment(experiment_id="")
+    assert str(ex.value) == (
+        "You must pass a valid experiment id, id provided was empty"
+    )
+
+    with pytest.raises(FailedActivity) as ex:
+        stop_experiment(experiment_id=None)
+    assert str(ex.value) == (
+        "You must pass a valid experiment id, id provided was empty"
+    )
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_stop_experiment_fails_if_exception_raised(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    client.stop_experiment.side_effect = Exception("Something went wrong")
+
+    with pytest.raises(FailedActivity) as ex:
+        stop_experiment(experiment_id="an-id")
+    assert str(ex.value) == "Stop Experiment failed, reason was: Something went wrong"
+
+
+@patch("chaosaws.fis.actions.aws_client", autospec=True)
+def test_that_stop_experiment_returns_client_response(aws_client):
+    client = MagicMock()
+    aws_client.return_value = client
+    resp = {"a-key": "a-value"}
+    client.stop_experiment.return_value = resp
+
+    actual_resp = stop_experiment(experiment_id="an-id")
     assert actual_resp == resp
