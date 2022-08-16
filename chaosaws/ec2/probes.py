046f121f6350e1e23823f6f6ca0e2092cee2c8f6
Deepesh Dhapola <dhapola@gmail.com>
2022-08-16 15:40:31 +0530

    updated comment to fix lint issue
    Signed-off-by: Deepesh Dhapola <dhapola@gmail.com>

--- chaosaws/ec2/probes.py
+++ chaosaws/ec2/probes.py
@@ -88,7 +88,8 @@ def count_min_instances(
     secrets: Secrets = None,
 ) -> bool:
     """
-    Returns whether minimum number of instances available matching the specified filters.
+    Returns whether minimum number of instances
+    available matching the specified filters.
 
     """
 
